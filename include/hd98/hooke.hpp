b289c8a9f1874c61ed9f3a7e4b745d0936bf6de9
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-02-05 20:46:32 +0100

    Make `stiffness_matrix` a private static method

--- include/hd98/hooke.hpp
+++ include/hd98/hooke.hpp
@@ -51,5 +51,19 @@ class Hooke {
       std::memcpy(C2, C.data(), HD98_SYM * HD98_SYM * sizeof(double));
     }
   }
+
+ private:
+  static std::array<double, HD98_SYM * HD98_SYM> stiffness_matrix(double lambda,
+                                                                  double mu) {
+    std::array<double, HD98_SYM * HD98_SYM> C{};
+    for (size_t i = 0, ij = 0; i < HD98_SYM; i++) {
+      for (size_t j = 0; j < HD98_SYM; j++, ij++) {
+        C[ij] = 0.0;
+        if (i == j) C[ij] += 2. * mu;
+        if ((i < HD98_DIM) && (j < HD98_DIM)) C[ij] += lambda;
+      }
+    }
+    return C;
+  }
 };
 }  // namespace hd98
