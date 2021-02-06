a72ceb6da14da880b410b593e8d9bba8f74c71ff
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-02-06 16:36:59 +0100

    Define new type aliases Tensor2 and Tensor4

--- tests/test_hd98.hpp
+++ tests/test_hd98.hpp
@@ -1,9 +1,13 @@
 #ifndef __TEST_HD98_H_20205717185754__
 #define __TEST_HD98_H_20205717185754__
 
+#include <array>
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+using Tensor2 = std::array<double, hd98::sym>;
+using Tensor4 = std::array<double, hd98::sym * hd98::sym>;
 
 void assert_true(bool predicate);
 void assert_equal(double act, double exp, double rtol, double atol);
